2f711bcc791ba017e15947a0c659f23acb168b87
Martijn <mbankert145@gmail.com>
2020-03-09 01:45:06 +0100

    Trying out SW stuff

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+//importScripts("./localforage.js");
+
 const main = document.querySelector('main');
 const dbName = "showcaseDB";
 const indexedDB = window.indexedDB ||
                   window.webkitIndexedDB ||
                   window.msIndexedDB ||
                   window.mozIndexedDB;
+                  
+let online = window.navigator.onLine;
 
 window.addEventListener('load', e => {
   loadShowcases();
@@ -11,8 +15,9 @@ window.addEventListener('load', e => {
   if('serviceWorker' in navigator) {
     try {
       navigator.serviceWorker.register('sw.js');
+      console.log(`SW registered`)
     } catch (error) {
-      console.log(`SW failed`);
+      console.log(`SW registration failed`);
     }
   }
 
@@ -21,72 +26,105 @@ window.addEventListener('load', e => {
   }else{
     console.log("Your browser supports IndexedDB")
   }
+
+  // if(online) {
+  //   fetch(`https://cmgt.hr.nl:8000/api/projects/tags`)
+  //   .then(response => {
+  //     return response.json();
+  //   })
+  //   .then(data => {
+  //     var text = "";
+
+  //     for(let i = 0; i < data.tags.length; i++) {
+  //       text += data.tags[i] + "<br>";
+  //     }
+  //     console.log(text)
+  //     //document.querySelector('.status').innerHTML = text;
+  //   })
+  // } else {
+  //   document.querySelector('.status').innerHTML = "The app is currently offline";
+  // }
 });
 
 async function loadShowcases() {
   console.log("meme")
   const res = await fetch(`https://cmgt.hr.nl:8000/api/projects/`);
   const json = await res.json();
+  console.log(json)
+  // let request = indexedDB.open(dbName, 1),
+  // db,
+  // tx,
+  // store,
+  // index;
 
-  let request = indexedDB.open(dbName, 1),
-  db,
-  tx,
-  store,
-  index;
+  json.projects.map(createProject);
 
-  main.innerHTML = json.projects.map(createProject).join('\n');
+  json.projects.forEach((project) => {
+    localforage.setItem(project._id, project).catch((err) =>{
+      console.log(err);
+    });
+  });
 
-  request.onupgradeneeded = function(e) {
-    let db = request.result,
-        store = db.createObjectStore("projectsStore", { keyPath: "_id"}),
-        index = store.createIndex("_id", "_id", { unique: true });
-  };
-  console.log("test1");
+  // request.onupgradeneeded = function(e) {
+  //   let db = request.result,
+  //       store = db.createObjectStore("projectsStore", { keyPath: "_id"}),
+  //       index = store.createIndex("_id", "_id", { unique: true });
+  // };
+  // console.log("test1");
 
-  request.onerror = function(e) {
-    console.log("There was an error: " + e.target.errorCode);
-  };
+  // request.onerror = function(e) {
+  //   console.log("There was an error: " + e.target.errorCode);
+  // };
 
-  request.onsuccess = function(e) {
-    console.log("test2");
-    db = request.result;
-    tx = db.transaction("projectsStore", "readwrite");
-    store = tx.objectStore("projectsStore");
-    index = store.index("_id");
+  // request.onsuccess = function(e) {
+  //   console.log("test2");
+  //   db = request.result;
+  //   tx = db.transaction("projectsStore", "readwrite");
+  //   store = tx.objectStore("projectsStore");
+  //   index = store.index("_id");
 
-    //get all errors when working with the database
-    db.onerror = function(e) {
-      console.log("ERROR" + e.target.errorCode);
-    }
+  //   //get all errors when working with the database
+  //   db.onerror = function(e) {
+  //     console.log("ERROR" + e.target.errorCode);
+  //   }
 
-    console.log(json)
-    //store all objects
-    for (let project in json.projects) {
-      store.put(json.projects[project]);
-    }
-    console.log("Added all projects to IndexedDB")
-    //close db
-    tx.oncomplete = function() {
-      db.close();
-      console.log("Close Database");
-    }
-  }
+  //   console.log(json)
+  //   //store all objects
+  //   for (let project in json.projects) {
+  //     store.put(json.projects[project]);
+  //   }
+  //   console.log("Added all projects to IndexedDB")
+  //   //close db
+  //   tx.oncomplete = function() {
+  //     db.close();
+  //     console.log("Close Database");
+  //   }
+  // }
 }
 
 function createProject(project) {
-  return `
-  <div class="container">
-    <div class="card-panel recipe white row">
-      <div class="project">
-        <a href="https://cmgt.hr.nl:8000/api/project/${project.slug}">
-          <h2>${project.title}</h2>
-          <img src="https://cmgt.hr.nl:8000/${project.headerImage}">
-          <p>${project.description}</p>
-          <hr/>
-          <p class="blue-text">${project.tags}</p>
-        </a>
-      </div>
-    </div>
-  </div>
-  `;
+  // return `;
+  localforage
+  .getItem(project._id)
+  .then(() => {
+    document.querySelector("main").insertAdjacentHTML(
+      "afterbegin",
+      `<div class="container">
+        <div class="card-panel recipe white row">
+          <div class="project">
+            <div>
+              <h2>${project.title}</h2>
+              <img src="https://cmgt.hr.nl:8000/${project.headerImage}">
+              <p>${project.description}</p>
+              <hr/>
+              <p class="blue-text">${project.tags}</p>
+            </div>
+          </div>
+        </div>
+      </div>`
+    );
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 }
--- sw.js
+++ sw.js
@@ -7,8 +7,6 @@ const cacheAssets = [
   './app.js',
   './fallback.json',
   './images/not-working.png',
-  'https://fonts.googleapis.com/icon?family=Material+Icons', 
-  'https://fonts.googleapis.com/css?family=Montserrat&display=swap',
   './localforage.js',
   'index.html',
   'test.html'
@@ -20,36 +18,81 @@ self.addEventListener('install', async e => {
   cache.addAll(cacheAssets);
 });
 
+self.addEventListener('activate', e => {
+  let cacheWhiteList = ['showcase-static']
+
+  e.waitUntil(
+    caches.keys().then (cacheNames => {
+      return Promise.all(
+        cacheNames.map (cacheName => {
+          if(cacheWhiteList.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName)
+          }
+        })
+      )
+    })
+  )
+})
+
 self.addEventListener('fetch', e => {
   const req = e.request;
   const url = new URL(req.url);
 
-  if(url.origin == location.origin) {
+  if(url.origin === location.origin) {
     console.log("chacheFirst")
     e.respondWith(cacheFirst(req));
+  // } else if (url.pathname === "/api/projects/tags/") {
+  //   e.respondWith(networkOnly(req));
   } else {
     console.log("networkFirst")
     e.respondWith(networkFirst(req));
   }
 });
 
+// async function cacheFirst(req) {
+//   const cachedResponse = await caches.match(req);
+//   console.log(cachedResponse)
+//   return cachedResponse || fetch(req);
+// } 
+
+// async function networkFirst(req) {
+//   const dynamicCache = await caches.open('showcase-dynamic');
+
+//   try {
+//     const res = await fetch(req);
+//     dynamicCache.put(req, res.clone());
+//     console.log("try")
+//     return res;
+//   } catch (error) {
+//     console.log("catch")
+//     const cachedResponse = await dynamicCache.match(req);
+//     return cachedResponse;
+//   }
+// }
+
 async function cacheFirst(req) {
-  const cachedResponse = await caches.match(req);
-  return cachedResponse || fetch(req);
-} 
+  const cachedResponse = await caches.match(req)
+  return cachedResponse || fetch(req)
+}
 
 async function networkFirst(req) {
-  const dynamicCache = await caches.open('showcase-dynamic');
+  const dynamicCache = await caches.open('showcase-dynamic-v1');
 
   try {
-    const res = await fetch(req);
-    dynamicCache.put(req, res.clone());
-    return res;
-    console.log("try")
+    const res = await fetch(req)
+    dynamicCache.put(req, res.clone())
+    return res
   } catch (error) {
-    console.log("catch")
-    const cachedResponse = await dynamicCache.match(req);
-    return cachedResponse;
+    const cachedRes = await dynamicCache.match(req)
+    return cachedRes
   }
 }
 
+async function networkOnly(req) {
+  try {
+    const res = await fetch(req)
+    return res
+  } catch (error) {
+    return error
+  }
+}
